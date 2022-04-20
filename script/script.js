69ea6d3ceb7b68d3835ca6d15b033da668293bdf
klizmagavrik <103041221+klizmagavrik@users.noreply.github.com>
2022-04-20 23:22:36 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
